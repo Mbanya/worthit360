3f12299a1ea588721427ff3e457af420cb2e5e06
mbanya <mbanyaw@gmail.com>
2017-05-20 12:28:01 +0300

    version 1.2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,180 @@
+@extends('layout.master')
+@section('assets')
+@endsection
+@section('header')
+@endsection
+@section('content')
+
+<!-- end .header-->
+        <div class="section-title-page area-bg area-bg_dark area-bg_op_75 parallax" style="background-size:cover; background-image: url(assets/media/general/andyone-26591.jpg)">
+            <div class="area-bg__inner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <ol class="breadcrumb">
+                                <li><a href="home.html">Home</a></li>
+                                <li class="active">About us</li>
+                            </ol>
+                            <!-- end breadcrumb-->
+                            <h1 class="b-title-page">who we are</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end .b-title-page-->
+        <main>
+            <section class="section-first section-first_type-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="section-first__carousel owl-carousel owl-theme enable-owl-carousel" data-pagination="true" data-navigation="false" data-single-item="true" data-auto-play="7000" data-transition-style="fade" data-main-text-animation="true" data-after-init-delay="3000" data-after-move-delay="1000" data-stop-on-hover="true">
+                                <div class="section-first__carousel-item">
+                                    <div class="section-first__img-group">
+                                        <a class="section-first__img bg-primary_a tilter tilter--1 js-zoom-images" href="assets/media/general/sabri-tuzcu-182689.jpg">
+                                            <figure class="tilter__figure"><img class="img-responsive" src="assets/media/general/sabri-tuzcu-182689.jpg" alt="foto" /></figure>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="section-first__carousel-item">
+                                    <div class="section-first__img-group">
+                                        <a class="section-first__img bg-primary_a tilter tilter--1 js-zoom-images" href="assets/media/general/roman-bozhko-251947.jpg">
+                                            <figure class="tilter__figure"><img class="img-responsive" src="assets/media/general/roman-bozhko-251947.jpg" alt="foto" /></figure>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="section-first__carousel-item">
+                                    <div class="section-first__img-group">
+                                        <a class="section-first__img bg-primary_a tilter tilter--1 js-zoom-images" href="assets/media/general/roman-bozhko-251947.jpg">
+                                            <figure class="tilter__figure"><img class="img-responsive" src="assets/media/general/roman-bozhko-251947.jpg" alt="foto" /></figure>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-7">
+                            <div class="section-first__inner">
+                                <div class="ui-decor-1"></div>
+                                <h2 class="ui-title-inner-2">We Are The WorthIT 360</h2>
+                                <div class="section-first__subtitle">Working-Hard to produce the contents that offers rich experience.</div>
+                                <p>Corem ipsum dolor sit amet consecter adipsicing elit sed uslm tempor incididunt magna aliqua ut dolor enim minim beniaps quis nostru alexercitation ulam laborisa sed bceaoan lorem ipsum minium euismode.</p>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <h3 class="section-first__title-link"><a class="btn btn-default" href="portfolio.html">history<i class="icon fa fa-long-arrow-right"></i></a></h3>
+                                        <p>Sit amet rem ipsum dolor amet lorem ipsum dolor amet consectetur adipisic elit do eiusmod tempor ncididuntut labore eturs dolore magna</p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h3 class="section-first__title-link"><a class="btn btn-default" href="portfolio.html">the mission<i class="icon fa fa-long-arrow-right"></i></a></h3>
+                                        <p>Sit amet rem ipsum dolor amet lorem ipsum dolor amet consectetur adipisic elit do eiusmod tempor ncididuntut labore eturs dolore magna</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- end .section-first-->
+            <section class="section-default">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h2 class="ui-title-bg ui-title-bg_sm">Our <br>statistics</h2>
+                            <ul class="b-progress-list b-progress-list_mod-a list-unstyled">
+                                <li class="b-progress-list__item">
+                                    <div class="b-progress-list__inner"><span class="b-progress-list__percent js-chart" data-percent="230"><span class="js-percent"></span></span><span class="b-progress-list__name">Satisfied Clients</span></div>
+                                </li>
+                                <li class="b-progress-list__item">
+                                    <div class="b-progress-list__inner"><span class="b-progress-list__percent js-chart" data-percent="407"><span class="js-percent"></span></span><span class="b-progress-list__name">Awards Won</span></div>
+                                </li>
+                                <li class="b-progress-list__item">
+                                    <div class="b-progress-list__inner"><span class="b-progress-list__percent js-chart" data-percent="637"><span class="js-percent"></span></span><span class="b-progress-list__name">Projectes Delivered</span></div>
+                                </li>
+                                <li class="b-progress-list__item">
+                                    <div class="b-progress-list__inner"><span class="b-progress-list__percent js-chart" data-percent="10"><span class="js-percent"></span><span class="b-progress-list__percent_label">K+</span></span><span class="b-progress-list__name">Lines of Code</span></div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="b-info b-info_mrg-top">
+                <div class="b-info__inner border-l_prim border-r_prim">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <h3 class="b-info__title">We always Work-Hard to produce the contents which offers the rich experiences for user.</h3>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="b-info__link"><a class="b-info__btn text-primary_h popup-youtube" href="http://www.youtube.com/watch?v=0O2aH4XLbto"><i class="icon pe-7s-play"></i>How We Work</a>
+                                    <div class="b-info__link-bg"><i class="icon pe-7s-video"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- end .b-info-->
+            <div class="section-default area-bg area-bg_dark area-bg_op_90 parallax" style="background-image: url(assets/media/content/bg/bg-1.jpg)">
+                <div class="area-bg__inner">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-10 col-md-offset-1">
+                                <div class="owl-carousel owl-theme owl-theme_pagin_act_prim enable-owl-carousel" data-pagination="true" data-navigation="false" data-single-item="true" data-auto-play="7000" data-transition-style="fade" data-main-text-animation="true" data-after-init-delay="3000" data-after-move-delay="1000" data-stop-on-hover="true">
+                                    <div class="b-blockquote b-blockquote-1 b-blockquote-1_dark">
+                                        <blockquote>
+                                            <cite class="b-blockquote__cite" title="Blockquote Title"><span class="b-blockquote__author">sam jones</span><span class="b-blockquote__category">Director</span></cite>
+                                            <p>Corem ipsum dolor sit amet consecter adipsicing elit sed usm tempor incididunt aret dolore magna aliqua dolor quis nostru alexercitation ulamco laborisa tempor sed bceaoan lorem ipsum dolor comoda sit amet ipsum dolor consecteuter lorem dolor sit amet consectetur adipisicing elit sed eiusmod tempor incididunt labore.</p>
+                                        </blockquote>
+                                    </div>
+                                    <!-- end .b-blockquote-->
+                                    <div class="b-blockquote b-blockquote-1 b-blockquote-1_dark">
+                                        <blockquote>
+                                            <cite class="b-blockquote__cite" title="Blockquote Title"><span class="b-blockquote__author">Olivia harper</span><span class="b-blockquote__category">Director</span></cite>
+                                            <p>Dolor sit amet consecter adipsicing elit sed usm tempor incididunt aret dolore magna aliqua dolor quis nostru alexercitation ulamco laborisa tempor sed bceaoan lorem ipsum dolor comoda sit amet ipsum dolor consecteuter lorem dolor sit amet consectetur adipisicing elit sed eiusmod tempor incididunt labore.</p>
+                                        </blockquote>
+                                    </div>
+                                    <!-- end .b-blockquote-->
+                                    <div class="b-blockquote b-blockquote-1 b-blockquote-1_dark">
+                                        <blockquote>
+                                            <cite class="b-blockquote__cite" title="Blockquote Title"><span class="b-blockquote__author">sara anderson</span><span class="b-blockquote__category">Director</span></cite>
+                                            <p>Sit amet consecter adipsicing elit sed usm tempor incididunt aret dolore magna aliqua dolor quis nostru alexercitation ulamco laborisa tempor sed bceaoan lorem ipsum dolor comoda sit amet ipsum dolor consecteuter lorem dolor sit amet consectetur adipisicing elit sed eiusmod tempor incididunt labore.</p>
+                                        </blockquote>
+                                    </div>
+                                    <!-- end .b-blockquote-->
+                                </div>
+                                <div class="text-center">
+                                    <div class="ui-decor-1"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-default">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="b-brands owl-carousel owl-theme enable-owl-carousel b-brands_bg-texture" data-min480="1" data-min768="3" data-min992="5" data-min1200="5" data-pagination="false" data-navigation="false" data-auto-play="4000" data-stop-on-hover="true">
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-1.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-2.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-3.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-4.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-5.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-1.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-2.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-3.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-4.png" alt="foto" /></div>
+                                <div class="b-brands__item"><img class="img-responsive center-block" src="assets/media/components/b-brands/logo-5.png" alt="foto" /></div>
+                            </div>
+                            <!-- end .b-brands-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+@endsection
+@section('footer')
+@endsection
+@section('scripts')
+@endsection
